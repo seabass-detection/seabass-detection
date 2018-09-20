b997aeb97fe4afe680158883460deb749c82efd3
seabass-detection <43419385+seabass-detection@users.noreply.github.com>
2018-09-20 20:43:56 +0100

    Update __init__.py

--- opencvlib/__init__.py
+++ opencvlib/__init__.py
@@ -1,3 +1,6 @@
+import os.path as _path
+import cv2 as _cv2
+
 IMAGE_EXTENSIONS = ('.bmp',
                     '.jpg',
                     '.jpeg',
@@ -25,3 +28,17 @@ __all__ = [
            'perspective'
            'roi',
            'transforms']
+
+
+
+def getimg(img, outflag=_cv2.IMREAD_UNCHANGED):
+    '''(ndarray|str)->ndarray
+    tries to load the image if its a path and returns the loaded ndarray
+    otherwise returns input img if it is an ndarray
+
+    Also consider using @decs._decs.decgetimg decorator
+    '''
+    if isinstance(img, str):
+        return _cv2.imread(_path.normpath(img), outflag)
+
+    return img
